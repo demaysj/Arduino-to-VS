5af5a2deb13ccdc9820fd277c76ff5e4bfb8d327
Sam DeMay <demaysj@appstate.edu>
2023-04-14 10:36:31 -0400

    Add project files.

---
+++ Arduino to VS/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Arduino_to_VS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.txtCurrVal = new System.Windows.Forms.TextBox();
+            this.cboPorts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(244, 21);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(453, 291);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // txtCurrVal
+            // 
+            this.txtCurrVal.Location = new System.Drawing.Point(133, 292);
+            this.txtCurrVal.Name = "txtCurrVal";
+            this.txtCurrVal.Size = new System.Drawing.Size(89, 20);
+            this.txtCurrVal.TabIndex = 1;
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.FormattingEnabled = true;
+            this.cboPorts.Location = new System.Drawing.Point(50, 21);
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(172, 21);
+            this.cboPorts.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Port:";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(24, 61);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(197, 22);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open Port";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(24, 89);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(197, 22);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close Port";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Current Acceleration:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(24, 117);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(197, 22);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear Chart";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 329);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboPorts);
+            this.Controls.Add(this.txtCurrVal);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox txtCurrVal;
+        private System.Windows.Forms.ComboBox cboPorts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Arduino to VS/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Arduino_to_VS
+{
+    public partial class Form1 : Form
+    {
+        SerialPort myPort;
+        string currDat;
+        int k = 0;
+
+        private delegate void ReadItDel(string text);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnClose.Enabled = false;
+
+            string[] ports = SerialPort.GetPortNames();
+            for (int i = 0; i < (ports.Length); i++)
+            {
+                cboPorts.Items.Add(ports[i]); //populate combobox with available ports
+            }
+            chart1.Series.Clear();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            while(chart1.Series.Count > 0) chart1.Series.RemoveAt(0); //get rid of everything in the chart
+            k = 0;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            cboPorts.Enabled = false;
+            btnOpen.Enabled = false;
+            btnClose.Enabled = true;
+
+            try
+            { 
+                myPort = new SerialPort(cboPorts.Text, 9600); //set baud to 9600 with odd parity and a stop bit
+                myPort.Parity = Parity.Odd;
+                myPort.StopBits = StopBits.One;
+                myPort.DataBits = 7;
+                myPort.Open();
+                myPort.DataReceived += new SerialDataReceivedEventHandler(myPort_DataReceived); //subscribe method to event handler
+                myPort.DiscardInBuffer(); //get rid of random numbers in buffer
+            }
+            catch (IOException ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void myPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if(myPort.IsOpen == true)
+            {
+                try
+                {
+                    Thread.Sleep(100); //give time to receive all data
+                    if(myPort.IsOpen == true) currDat = myPort.ReadLine(); //read current data line if the port is still open
+                    this.BeginInvoke(new ReadItDel(DisplayAcc), new object[] { currDat }); //call display function using delegate
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void DisplayAcc(string accdat)
+        {
+            txtCurrVal.Text = accdat;
+            if (chart1.Series.Count == 0) //if the chart doesnt have a series make one
+            {
+                chart1.Series.Add("Acc. Data");
+                chart1.Series["Acc. Data"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastPoint;
+            }
+            if (k >= 50) chart1.Series["Acc. Data"].Points.RemoveAt(0); //if there are more than 50 points remove the first one
+            chart1.Series["Acc. Data"].Points.AddXY(k, accdat); //add current data
+            chart1.ChartAreas[0].RecalculateAxesScale(); //rescale axes
+            k++; //increment k
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            btnClose.Enabled = false;
+            try
+            {
+                myPort.Close();
+                btnOpen.Enabled = true; //enable open button combobox for port selection
+                cboPorts.Enabled = true;
+            }
+            catch { }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                myPort.Close();
+                if (myPort != null) myPort.Dispose(); //dispose of the port upon closing
+            }
+            catch { }
+        }
+    }
+}
